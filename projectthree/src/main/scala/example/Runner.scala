31d0455118971e6746977f3506329cd059269e48
Frank Rauen <home@Franks-MacBook-Air.local>
2021-02-26 07:33:46 -0700

    adds df

--- projectthree/src/main/scala/example/Runner.scala
+++ projectthree/src/main/scala/example/Runner.scala
@@ -7,22 +7,25 @@ object Runner {
     val spark = SparkSession
       .builder()
       .appName("scalas3read")
-      .master("local[4]")
+      .master("local[*]")
       .getOrCreate()
 
-    // Reference: https://sparkbyexamples.com/spark/spark-read-text-file-from-s3/#s3-dependency
-    val key = System.getenv(("DAS_KEY_ID"))
-    val secret = System.getenv(("DAS_SEC"))
+    val key = System.getenv("AWS_ACCESS_KEY_ID")
+    val secret = System.getenv("AWS_SECRET_ACCESS_KEY")
 
     spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", key)
     spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", secret)
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
-    
+
     import spark.implicits._
     spark.sparkContext.setLogLevel("WARN")
 
-      val s3DataMaybe = spark.sparkContext.textFile("s3a://commoncrawl/crawl-data/CC-MAIN-2013-48/segments/1386163035819/warc/CC-MAIN-20131204131715-00000-ip-10-33-133-15.ec2.internal.warc.gz")
-      s3DataMaybe.take(2000).foreach(println)
+    val df = spark.read.load("s3a://commoncrawl/cc-index/table/cc-main/warc/")
 
+    df
+    .select("url_host_name", "url_path")
+    .filter($"crawl" === "CC-MAIN-2020-16")
+    .filter($"subset" === "warc")
+    .filter($"url_path".contains("job"))
+    .show(200, false)
   }
 }
